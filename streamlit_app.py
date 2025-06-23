f4815a7d1b3a2eaa6ab05ccf0ef229b656fc22e4
Breno Oliveira <breno.oliveira251@outlook.com>
2025-06-23 17:10:48 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,7 +67,7 @@ html_code = f"""
   html, body {{
     margin: 0; padding: 0;
     height: 100vh;
-    background: #fefefe;
+    background: #f4cccc;
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
       Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
     color: #333;
