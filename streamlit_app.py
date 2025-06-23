a959b3dea62089e5a80f486d9e2554d90538b0ad
Breno Oliveira <breno.oliveira251@outlook.com>
2025-06-23 15:50:56 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -85,7 +85,7 @@ html_code = f"""
     box-sizing: border-box;
     overflow-y: hidden; /* tira scroll */
     border-radius: 10px 0 0 10px;
-    box-shadow: inset 3px 0 6px -3px rgba(0, 0, 0, 0.075);
+    box-shadow: inset 3px 0 6px -3px rgba(0, 0, 0, 0.06);
     font-size: 12px;
     line-height: 1.25;
     color: #555;
@@ -116,7 +116,7 @@ html_code = f"""
 
   #legend div:hover {{
     background-color:;
-    color: #1a1a1a;
+    color: #4a4a4a;
   }}
 
   #legend div.active {{
