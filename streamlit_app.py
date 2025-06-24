3263caf8fa5aee4f28410ded213df91fb7096463
Breno Oliveira <breno.oliveira251@outlook.com>
2025-06-23 22:34:03 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import json
 from pathlib import Path
 
 # Configurações da página Streamlit
-st.set_page_config(page_title="RMC Data", layout="wide", initial_sidebar_state="collapsed")
+st.set_page_config(page_title="RMC Ddata", layout="wide", initial_sidebar_state="collapsed")
 
 # Título e descrição
 st.title("RMC Data")
