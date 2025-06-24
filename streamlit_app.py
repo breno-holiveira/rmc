97c1e574f581710c8db21c53816402a7938fe640
Breno Oliveira <breno.oliveira251@outlook.com>
2025-06-24 19:57:31 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,7 +88,7 @@ html_code = f"""
     border-radius: 8px;
     cursor: pointer;
     user-select: none;
-    font-size: 14px;
+    font-size: 15px;
     line-height: 1.3;
     color: #1a2d5a;
     transition: background-color 0.3s, color 0.3s;
@@ -153,7 +153,7 @@ html_code = f"""
     border-radius: 10px;
     box-shadow: 0 1px 6px rgba(0,0,0,0.1);
     max-width: 320px;
-    font-size: 16px;
+    font-size: 14px;
     line-height: 1.3;
     color: #1a2d5a;
     user-select: none;
