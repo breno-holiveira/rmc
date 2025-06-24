784e05b265e754d50e343378362c4701c637e913
Breno Oliveira <breno.oliveira251@outlook.com>
2025-06-23 22:44:18 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,7 +88,7 @@ html_code = f"""
     width: 210px;
     background: #fff;
     padding: 16px 20px;
-    box-shadow: 2px 0 8px rgba(0,0,0,0.08);
+    box-shadow: 2px 0 8px rgba(0,0,0,0.5);
     border-right: 1px solid #e3e6ea;
     overflow-y: auto;
   }}
