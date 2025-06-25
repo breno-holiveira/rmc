f8d2942d923da1fca388b47bc840343ea4988797
Breno Oliveira <breno.oliveira251@outlook.com>
2025-06-25 20:38:51 -0300

    Update pag1.py

--- pages/pag1.py
+++ pages/pag1.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-st.set_page_config(page_title="Página 1", layout="wide", initial_sidebar_state="collapsed")
-
-st.title("Página 1")
-st.markdown("Conteúdo da Página 1")
+def main():
+    st.title("Página 1")
+    st.markdown("Conteúdo da Página 1")
