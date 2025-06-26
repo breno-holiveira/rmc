279b6f0b01d8f3d955793068db663551b16b639e
Breno Oliveira <breno.oliveira251@outlook.com>
2025-06-25 21:13:16 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,25 @@ import streamlit as st
 st.set_page_config(layout="wide")
 st.markdown("<style>div[data-testid='stSidebar']{display:none !important;}</style>", unsafe_allow_html=True)
 
-with st.expander("Menu"):
-    option = st.radio("Escolha a página:", ["Início", "Página 1", "Página 2"])
+if "page" not in st.session_state:
+    st.session_state.page = "home"
 
-if option == "Início":
+cols = st.columns([1,1,1])
+
+with cols[0]:
+    if st.button("Início"):
+        st.session_state.page = "home"
+with cols[1]:
+    if st.button("Página 1"):
+        st.session_state.page = "pag1"
+with cols[2]:
+    if st.button("Página 2"):
+        st.session_state.page = "pag2"
+
+if st.session_state.page == "home":
     st.title("Início")
     st.write("Conteúdo da página inicial.")
-elif option == "Página 1":
+elif st.session_state.page == "pag1":
     st.title("Página 1")
     st.write("Conteúdo da página 1.")
 else:
