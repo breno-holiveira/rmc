54fcab442f9153b132cff4874a01407ed322d07b
Breno Oliveira <breno.oliveira251@outlook.com>
2025-06-27 13:01:15 -0300

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -8,24 +8,23 @@ def show_home():
     da Região Metropolitana de Campinas (RMC), visando garantir a qualidade e a confiabilidade das informações, além de contribuir para a ampliação da 
     análise e da divulgação dos dados em nível regional e municipal.
     
-    O projeto foi desenvolvido por **Breno Oliveira** e **Bianca Lopes**, utilizando Python e a biblioteca Streamlit. Todo o conteúdo disponível pode ser
-    reproduzido, adaptado ou redistribuído, parcial ou integralmente, sem necessidade de autorização prévia.
+    O projeto foi desenvolvido por Breno Oliveira e Bianca Lopes. Todo o conteúdo disponível pode ser reproduzido, adaptado ou redistribuído, parcial ou
+    integralmente, sem necessidade de autorização prévia.
     
-    Em caso de dúvidas ou sugestões, entre em contato por meio da aba **Contatos**.
+    Em caso de dúvidas ou sugestões, entre em contato por meio da aba Contatos.
     """)
     
     st.markdown('# Região Metropolitana de Campinas')
     
     st.markdown(
         """
-    A Região Metropolitana de Campinas (RMC) foi criada em 2000, por meio da Lei Complementar nº 870, do Estado de São Paulo. Composta por 20 municípios, a 
+    A Região Metropolitana de Campinas foi criada em 2000, por meio da Lei Complementar nº 870, do Estado de São Paulo. Composta por 20 municípios, a 
     RMC se destaca pela alta qualidade de vida e pelo constante desenvolvimento social e econômico.
     
     Entre seus principais destaques:
-    
-    - Em 2023, Campinas foi reconhecida oficialmente como a **Capital Nacional da Ciência, Tecnologia e Inovação** (Lei nº 3.680/23).
-    - A região abriga **6 cidades entre as 30 mais seguras do Brasil** em 2024, segundo dados do IBGE e do Ministério da Saúde — com **Valinhos** ocupando o 1º lugar no ranking nacional.
-    - Campinas é a **única metrópole brasileira que não é capital de estado**, de acordo com a REGIC 2018 (IBGE).
-    - Em 2025, Campinas foi eleita a **melhor cidade do interior do país para investimentos**, segundo o *Oxford Economics Global Cities Index*.
+    - Campinas é oficialmente reconhecida como a Capital Nacional da Ciência, Tecnologia e Inovação, conforme o Projeto de Lei 3680/23.
+    - A região abriga 6 municípios entre os 30 mais seguros do Brasil, com Valinhos ocupando a 1ª posição, segundo dados do IBGE e do Ministério da Saúde.
+    - Campinas é a única cidade do interior do Brasil a possuir o status de metrópole, segundo o IBGE.
+    - Em 2025, Campinas foi eleita a melhor cidade do interior do país para investimentos, de acordo com a *Oxford Economics Global Cities Index*.
         """
     )
