b51f9d0b3a518fcb2bc136cdb4c123f771230d4e
Breno Oliveira <breno.oliveira251@outlook.com>
2025-06-27 09:32:45 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from streamlit_navigation_bar import st_navbar
 
+
 st.set_page_config(initial_sidebar_state="collapsed")
 
 pages = ["Home", "Library", "Tutorials", "Development", "Download"]
-
 styles = {
     "nav": {
         "background-color": "rgb(123, 209, 146)",
@@ -15,13 +15,11 @@ styles = {
     "span": {
         "border-radius": "0.5rem",
         "color": "rgb(49, 51, 63)",
-        "margin": "0 0.25rem",
-        "padding": "0.5rem 0.75rem",
-        "transition": "background-color 0.3s ease",
+        "margin": "0 0.125rem",
+        "padding": "0.4375rem 0.625rem",
     },
     "active": {
         "background-color": "rgba(255, 255, 255, 0.25)",
-        "font-weight": "bold",
     },
     "hover": {
         "background-color": "rgba(255, 255, 255, 0.35)",
