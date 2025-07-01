8679bee335fba4b82a4bca6935a105740ecb1923
Breno Oliveira <breno.oliveira251@outlook.com>
2025-07-01 14:11:50 -0300

    Delete pages/finanças.py

--- pages/finanças.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title("Finanças")
-st.write("Nesta seção você encontrará dados financeiros.")
-
-st.subheader("Subtópicos")
-st.page_link("pages/despesas.py", label="▶️ Despesas")
-st.page_link("pages/arrecadacao.py", label="▶️ Arrecadação")
