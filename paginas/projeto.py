f368b7955b7e668d3925b69999d9e3180d66e722
Breno Oliveira <breno.oliveira251@outlook.com>
2025-07-30 11:30:29 -0300

    Update projeto.py

--- paginas/projeto.py
+++ paginas/projeto.py
@@ -18,13 +18,13 @@ st.markdown(
     '''
 )
 
-col1, col2, col3 = st.columns([1, 2, 1])  # Coluna central maior para centralizar imagem
+col1, col2, col3 = st.columns([1, 2, 1])
 
 with col1:
-    st.write(' ')  # Espaço vazio
+    st.write("")
 
 with col2:
-    st.image("arquivos/logo-pucc.png")  # Imagem centralizada na coluna do meio
+    st.image("arquivos/logo-pucc.png")
 
 with col3:
-    st.write(' ')  # Espaço vazio
+    st.write("")
