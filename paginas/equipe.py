0125557ff5de97bfc9ab723b252360edef0cef4b
Breno Oliveira <breno.oliveira251@outlook.com>
2025-07-30 12:03:36 -0300

    Update equipe.py

--- paginas/equipe.py
+++ paginas/equipe.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.markdown('## Equipe')
 
-st.markdown('**Orientadora:** Profa. Dra. Daniela Scarpa Beneli - [🔍︎ Lattes](http://lattes.cnpq.br/3699082804969492)')
+st.markdown('**Orientadora:** Profa. Dra. Daniela Scarpa Beneli -- [🔍︎ Lattes](http://lattes.cnpq.br/3699082804969492)')
