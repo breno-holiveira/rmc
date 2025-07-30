f10d25edeb90111faaaf1c65834f0c08c9bb1838
Breno Oliveira <breno.oliveira251@outlook.com>
2025-07-30 09:53:22 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,14 @@ import streamlit as st
 
 pages = {
     "Sobre": [
-        st.Page("projeto.py", title="Projeto de Pesquisa"),
-        st.Page("integrantes.py", title="Integrantes"),
+        st.Page("paginas/projeto.py", title="Projeto de Pesquisa"),
+        st.Page("paginas/integrantes.py", title="Integrantes"),
     ],
     "Dados e Resultados": [
-        st.Page("metodologia.py", title="Metodologia"),
-        st.Page("municipais.py", title="Dispêndios Municipais em C&T"),
-        st.Page("estaduais.py", title="Dispêndios Estaduais em C&T"),
-        st.Page("documentos.py", title="Referências e Documentos"),
+        st.Page("paginas/metodologia.py", title="Metodologia"),
+        st.Page("paginas/municipais.py", title="Dispêndios Municipais"),
+        st.Page("paginas/estaduais.py", title="Dispêndios Estaduais"),
+        st.Page("paginas/documentos.py", title="Referências e Documentos"),
     ],
 }
 
