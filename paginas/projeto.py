07ada77bd119926526115b7bfcbbd695113b9278
Breno Oliveira <breno.oliveira251@outlook.com>
2025-07-30 10:22:39 -0300

    Update projeto.py

--- paginas/projeto.py
+++ paginas/projeto.py
@@ -1,3 +1,15 @@
 import streamlit as st
 
-st.markdown('## Projeto de Pesquisa')
+st.markdown('## Dispêndio Público em Ciência em Tecnologia no Município de Campinas (2016-2024)')
+
+st.markdown(
+    '''
+    O projeto de pesquisa visa mensurar os dispêndios públicos municipais e estaduais em Ciência e Tecnologia (C&T) na cidade de Campinas/SP, no período de 2016 a 2024.
+
+    Para isso, foram adotadas [metodologias](./metodologia). reconhecidas nacional e internacionalmente, buscando analisar de forma rigorosa os investimentos e seu impacto no desenvolvimento local.
+
+    Navegue pelas seções do site para conhecer a equipe, a metodologia aplicada, os dados coletados e as análises realizadas.
+
+    Esperamos que este trabalho contribua para a compreensão e o aprimoramento das políticas públicas em C&T na região.
+    '''
+)
