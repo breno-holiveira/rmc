afa02df2272e0cdc247e8b6c3e7fa0a038434683
Breno Oliveira <breno.oliveira251@outlook.com>
2025-07-30 14:54:39 -0300

    Update equipe.py

--- paginas/equipe.py
+++ paginas/equipe.py
@@ -6,15 +6,7 @@ st.markdown('**Orientadora:** Prof.ª Dr.ª Daniela Scarpa Beneli -- 🔍︎[Lat
 st.markdown('**Orientando:** Breno Henrique de Oliveira -- [Currículo Lattes](http://lattes.cnpq.br/1654783111555271)')
 
 st.markdown(
-    '''
-    <p>
-      <strong>Orientadora:</strong>
-      Prof.ª Dr.ª Daniela Scarpa Beneli — 
-      <a href="http://lattes.cnpq.br/3699082804969492" style="text-decoration: none; color: inherit; font-size: 16px;">
-        🔍︎ Currículo Lattes
-      </a>
-    </p>
-    ''',
+    '<a href="http://lattes.cnpq.br/3699082804969492" style="text-decoration: none; color: inherit;">🔍︎ <span style="font-size: 16px;">Currículo Lattes</span></a>',
     unsafe_allow_html=True
 )
 
