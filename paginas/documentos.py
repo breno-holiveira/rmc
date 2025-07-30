b120b0fdbb9dfc3f43a2395ab50d9dfe0af15230
Breno Oliveira <breno.oliveira251@outlook.com>
2025-07-30 15:32:10 -0300

    Update documentos.py

--- paginas/documentos.py
+++ paginas/documentos.py
@@ -2,12 +2,16 @@ import streamlit as st
 
 st.markdown('## Referências e Documentos')
 
-st.markdown('Clique em um dos links abaixo para acessar os documentos em formato PDF.')
+st.markdown('### Arquivos em PDF')
 
 st.markdown(
     """
-    - 📄 <a href="https://www.gov.br/mcti/pt-br/acompanhe-o-mcti/indicadores/paginas/referencias/manuais-de-referencia/arquivos/OCDE_ManualFrascati_2015_PT_BR.pdf" target="_blank" download>Manual de Frascati 2015 (OCDE)</a>
-    - 📄 <a href="https://antigo.mctic.gov.br/mctic/export/sites/institucional/indicadores/detalhe/Manuais/RIECTI-Manual-da-Rede-de-Indicadores-Estaduais-de-CT-2015.pdf" target="_blank" download>Instruções para a Mensuração de C&T (RIECTI)</a>
+    - <a href="https://www.gov.br/mcti/pt-br/acompanhe-o-mcti/indicadores/paginas/referencias/manuais-de-referencia/arquivos/OCDE_ManualFrascati_2015_PT_BR.pdf" target="_blank" download>Manual de Frascati 2015 (OCDE)</a>
+    - <a href="https://antigo.mctic.gov.br/mctic/export/sites/institucional/indicadores/detalhe/Manuais/RIECTI-Manual-da-Rede-de-Indicadores-Estaduais-de-CT-2015.pdf" target="_blank" download>Instruções para a Mensuração de C&T (RIECTI)</a>
     """,
     unsafe_allow_html=True
 )
+
+st.markdown('### Sites')
+
+
