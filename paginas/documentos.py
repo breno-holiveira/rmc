97f465fbbf92f1ae7c35b5aa8a27cfe8abcbc5bd
Breno Oliveira <breno.oliveira251@outlook.com>
2025-07-30 15:23:35 -0300

    Update documentos.py

--- paginas/documentos.py
+++ paginas/documentos.py
@@ -3,8 +3,8 @@ import streamlit as st
 st.markdown('## Referências e Documentos')
 
 st.markdown(
-    '<a href="https://www.gov.br/mcti/pt-br/acompanhe-o-mcti/indicadores/paginas/referencias/manuais-de-referencia/arquivos/OCDE_ManualFrascati_2015_PT_BR.pdf" '
-    'style="text-decoration: none; color: #1f77b4;" target="_blank" download>'
-    '<span style="font-size: 16px;">Manual de Frascati 2015 (PDF)</span></a>',
+    '<a href="https://www.gov.br/mcti/pt-br/acompanhe-o-mcti/indicadores/paginas/referencias/manuais-de-referencia/arquivos/OCDE_ManualFrascati_2015_PT_BR.pdf" target="_blank" download>'
+    'Download Manual Frascati (PDF)'
+    '</a>',
     unsafe_allow_html=True
 )
