73844b4e612e634ae2918bad69f09d4a86c6820d
Breno Oliveira <breno.oliveira251@outlook.com>
2025-07-30 15:09:16 -0300

    Update equipe.py

--- paginas/equipe.py
+++ paginas/equipe.py
@@ -17,3 +17,15 @@ st.markdown(
     '<span style="font-size: 16px;">🔍︎ Lattes</span></a>',
     unsafe_allow_html=True
 )
+
+st.markdown(
+    '**Orientadora:** Prof.ª Dr.ª Daniela Scarpa Beneli \u2002'
+    '<a href="http://lattes.cnpq.br/3699082804969492" '
+    'style="text-decoration: none; color: #1f77b4;">'
+    '<span style="font-size: 16px;">🔍︎ Lattes</span></a>'
+    '\u2003'  # espaço largo entre links
+    '<a href="mailto:daniela.beneli@universidade.edu.br" '
+    'style="text-decoration: none; color: #1f77b4;">'
+    '<span style="font-size: 16px;">✉️ Email</span></a>',
+    unsafe_allow_html=True
+)
