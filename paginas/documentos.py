818abebb612294fffa31364c2b9f4be0b6db4888
Breno Oliveira <breno.oliveira251@outlook.com>
2025-07-30 15:24:10 -0300

    Update documentos.py

--- paginas/documentos.py
+++ paginas/documentos.py
@@ -4,7 +4,7 @@ st.markdown('## Referências e Documentos')
 
 st.markdown(
     '<a href="https://www.gov.br/mcti/pt-br/acompanhe-o-mcti/indicadores/paginas/referencias/manuais-de-referencia/arquivos/OCDE_ManualFrascati_2015_PT_BR.pdf" target="_blank" download>'
-    'Download Manual Frascati (PDF)'
+    'Manual de Frascati 2015 (PDF)'
     '</a>',
-    unsafe_allow_html=True
+    unsafe_allow_html=False
 )
