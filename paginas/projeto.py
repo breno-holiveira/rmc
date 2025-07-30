91b001ddaeb3d7e609b06b5dfdd0de0b5fa13cf0
Breno Oliveira <breno.oliveira251@outlook.com>
2025-07-30 10:36:16 -0300

    Update projeto.py

--- paginas/projeto.py
+++ paginas/projeto.py
@@ -4,9 +4,9 @@ st.markdown('## Dispêndio Público em Ciência em Tecnologia no Município de C
 
 st.markdown(
     '''
-    O projeto de pesquisa, fruto de uma Iniciação Científica financiada pela Pontifícia Universidade Católica de Campinas (PUC-Campinas), tem como objetivo mensurar
-    dispêndios públicos municipais e estaduais em Ciência e Tecnologia (C&T) no município de Campinas/SP, no período de 2016 a 2024.
+    O projeto de pesquisa, desenvolvido através de uma Iniciação Científica financiada pela Pontifícia Universidade Católica de Campinas (PUC-Campinas), tem como 
+    objetivo mensurar os dispêndios públicos municipais e estaduais em Ciência e Tecnologia (C&T) no município de Campinas/SP, durante o período de 2016 a 2024.
 
-    Para isso, foram utilizadas metodologias reconhecidas nacional e internacionalmente, como o Manual de Frascati (OCDE) e instruções da (RIECTI).
+    Para consultar os resultados, a metodologia e o material utilizado, navegue pelo menu lateral.
     '''
 )
